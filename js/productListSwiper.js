3577c6934a2310f5460dae36877781ce555655ff
born-here <choi.hyeonjee26@gmail.com>
2025-03-11 03:06:42 +0900

    route된 {page}.html마다 각각의 js가 적용되도록 fix

---
+++ js/productListSwiper.js
@@ -0,0 +1,27 @@
+function initSwiper() {
+  if (window.productSwiper) {
+    window.productSwiper.destroy();
+  }
+
+  window.productSwiper = new Swiper('.grid-container', {
+    wrapperClass: 'grid-wrapper-inner',
+    slideClass: 'grid-page',
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+    },
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+    speed: 600,
+    spaceBetween: 30,
+    slidesPerView: 1,
+    effect: 'fade',
+    fadeEffect: {
+      crossFade: true,
+    },
+  });
+}
+
+export { initSwiper };
