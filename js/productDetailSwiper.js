ad045cda4983cdb571b59d70a130f796a638546c
oloklkl <sungju7694@naver.com>
2025-03-13 02:53:17 +0900

    productDetailSwiper.js 추가,수정

--- js/productDetailSwiper.js
+++ js/productDetailSwiper.js
@@ -1,3 +1,40 @@
+document.addEventListener('DOMContentLoaded', () => {
+    new Swiper('.product-slider-section .swiper', {
+        // 제품 상세 Swiper
+        slidesPerView: 3,
+        centeredSlides: true,
+        loop: true,
+        spaceBetween: 10,
+        navigation: {
+            nextEl: '.product-slider-section .swiper-button-next',
+            prevEl: '.product-slider-section .swiper-button-prev',
+        },
+    });
+
+    new Swiper('.related-products-section .swiper', {
+        // 관련 제품 Swiper
+        slidesPerView: 1.2,
+        spaceBetween: 20,
+        loop: true,
+        navigation: {
+            nextEl: '.related-products-section .swiper-button-next',
+            prevEl: '.related-products-section .swiper-button-prev',
+        },
+        pagination: {
+            el: '.related-products-section .swiper-pagination',
+            clickable: true,
+        },
+        breakpoints: {
+            768: {
+                slidesPerView: 2.5,
+            },
+            1024: {
+                slidesPerView: 3,
+            },
+        },
+    });
+});
+
 document.addEventListener('DOMContentLoaded', function () {
     new Swiper('.related-products', {
         slidesPerView: 1.2, // 한 번에 보이는 슬라이드 개수
