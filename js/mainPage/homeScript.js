27b08fbfe6bdc084087fea645c1ab5d53f1fa24d
born-here <choi.hyeonjee26@gmail.com>
2025-03-13 14:12:12 +0900

    디자인) home 스타일 작업

--- js/mainPage/homeScript.js
+++ js/mainPage/homeScript.js
@@ -1,3 +1,21 @@
 export function initializePage() {
   console.log('✅ 홈 페이지 초기화 실행됨!');
 }
+document.addEventListener('DOMContentLoaded', function () {
+  gsap.registerPlugin(ScrollTrigger);
+
+  gsap.utils.toArray('.letter-img').forEach((img) => {
+    let speed = img.dataset.speed; // HTML에서 data-speed 값을 가져옴
+
+    gsap.to(img, {
+      y: -window.innerHeight * speed, // 스크롤 시 이미지가 위로 이동하는 정도 조절
+      ease: 'power1.out',
+      scrollTrigger: {
+        trigger: '.section01',
+        start: 'top bottom',
+        end: 'bottom top',
+        scrub: true,
+      },
+    });
+  });
+});
