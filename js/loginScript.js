517d0d90102f9816836c089335b70d7429c98783
hyeonky <hyeon2762@naver.com>
2025-03-17 02:11:34 +0900

    작업 ) 로그인 연동, 마이페이지 로그인 상태에 따른 메뉴 ui 변경, index.html fetch함수 마이페이지, 검색창 추가  수정) 마이페이지 스타일 변경경

--- js/loginScript.js
+++ js/loginScript.js
@@ -1,10 +1,9 @@
 // 로그인 버튼 클릭 시
 loginForm.addEventListener('submit', function (e) {
   e.preventDefault()
-  console.log('✅ 로그인 버튼 클릭됨')
 
-  const userId = document.getElementById('userId')?.value.trim()
-  const password = document.getElementById('password')?.value.trim()
+  const userId = document.getElementById('userId')?.value
+  const password = document.getElementById('password')?.value
 
   console.log('입력된 ID:', userId)
   console.log('입력된 PW:', password)
@@ -29,21 +28,29 @@ loginForm.addEventListener('submit', function (e) {
     return
   }
 
-  // 로그인 성공 처리
+  // 로그인 성공&실패
   if (userId === 'popmart' && password === 'p1234!') {
     alert('로그인 성공!')
     console.log('✅ 로그인 성공')
 
-    // 로그인 정보 로컬 스토리지에 저장
+    // 로그인 후 localStorage에 로그인 상태 저장
     localStorage.setItem('isLoggedIn', 'true')
     localStorage.setItem('userId', userId)
 
-    // 로그인 후 페이지 이동
     window.location.hash = '#home'
   } else {
-    // 로그인 실패 처리
     document.getElementById('passwordError').textContent =
       '아이디 또는 비밀번호가 올바르지 않습니다.'
     document.getElementById('passwordError').classList.add('visible')
+  }
+})
+
+// 로그인 상태 확인 및 처리
+window.addEventListener('load', function () {
+  const isLoggedIn = localStorage.getItem('isLoggedIn')
+  if (isLoggedIn === 'true') {
+    // 로그인 상태인 경우
+    alert('이미 로그인 되어 있습니다.')
+    window.location.hash = '#home'
   }
 })
