4b5b7a69c8c75bded4f38f11358065ae9ac010f9
hyeonky <hyeon2762@naver.com>
2025-03-17 16:32:46 +0900

    주석) test헤더 주석 처리 추가) 검색창 닫기 기능 추가

--- js/searchDrawerScript.js
+++ js/searchDrawerScript.js
@@ -1,7 +1,14 @@
 document.getElementById('search-btn').addEventListener('click', function () {
   document.getElementById('search-box').classList.toggle('hidden')
+  document.getElementById('modal-overlay').classList.toggle('hidden')
 })
 
+// 창을 닫는 기능을 하나의 함수로 통합
 function closeSearch() {
   document.getElementById('search-box').classList.add('hidden')
+  document.getElementById('modal-overlay').classList.add('hidden')
 }
+
+// 배경 클릭 시 또는 X 버튼 클릭 시 창을 닫는 이벤트 처리
+document.getElementById('modal-overlay').addEventListener('click', closeSearch)
+document.getElementById('close').addEventListener('click', closeSearch)
