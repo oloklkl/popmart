6b58951d9e735de7da3214bf75909b33685bc523
hyeonky <hyeon2762@naver.com>
2025-03-17 18:15:42 +0900

    수정) 검색창 스타일&기능 수정 중

--- js/searchDrawerScript.js
+++ js/searchDrawerScript.js
@@ -1,12 +1,12 @@
 document.getElementById('search-btn').addEventListener('click', function () {
-  document.getElementById('search-box').classList.toggle('hidden')
-  document.getElementById('modal-overlay').classList.toggle('hidden')
+  document.getElementById('search-box').classList.toggle('open')
+  document.getElementById('modal-overlay').classList.toggle('open')
 })
 
 // 창을 닫는 기능을 하나의 함수로 통합
 function closeSearch() {
-  document.getElementById('search-box').classList.add('hidden')
-  document.getElementById('modal-overlay').classList.add('hidden')
+  document.getElementById('search-box').classList.remove('open')
+  document.getElementById('modal-overlay').classList.remove('open')
 }
 
 // 배경 클릭 시 또는 X 버튼 클릭 시 창을 닫는 이벤트 처리
