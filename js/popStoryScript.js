2e2db0180e3e4afce1355aabb945bb20ddbb4626
eunju98 <dmswn1321@gmail.com>
2025-03-20 13:07:59 +0900

    팝스토리 아티스트정보 부분 효과부여

---
+++ js/popStoryScript.js
@@ -0,0 +1,9 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const artistInfo = document.querySelector(".artist-info");
+    const artistText = document.querySelector(".artist-p");
+  
+    artistInfo.addEventListener("click", () => {
+      artistInfo.classList.toggle("active");
+    });
+  });
+  
